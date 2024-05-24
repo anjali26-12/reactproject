f8914f8ea6d7a14c8d2b9757d814613d9b80e0af
anjali26-12 <anju7022tiwari@gmail.com>
2024-05-24 15:59:53 +0000

    entered my name

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anjali Tiwari",
+  title: "Web Developer",
+  email: "anju7022tiwari@gmail.com",
+  gitHub: "anjali26-12",
+  instagram: "anjali26",
+  linkedIn: "anjalitiwari",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
